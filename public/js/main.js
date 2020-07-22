571237a3e22317adc54adff1844f6f744f058ffd
freeman1306 <zavalkin06@gmail.com>
2020-07-22 21:42:25 +0300

    go further

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,25 @@
 const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
 
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
 
 const socket = io()
+
+
+// Join chatroom
+socket.emit('joinRoom', {username, room})
+
+
 
 socket.on('message', message => {
     outputMessage(message)
 
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight
 })
 
 
@@ -19,6 +33,9 @@ chatForm.addEventListener('submit', (e) => {
     // Emit message to server
     socket.emit('chatMessage', msg)
 
+    // Clear input
+    e.target.elements.msg.value = ''
+    e.target.elements.msg.focus()
 })
 
 
@@ -27,9 +44,9 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message) {
     const div = document.createElement('div')
     div.classList.add('message')
-    div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
                        <p class="text">
-                          ${message}                     
+                          ${message.text}                     
                        </p>`
-    document.querySelector('.chat-messages').appendChild(div)
+    chatMessages.appendChild(div)
 }
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser} = require('.//utils/users')
 
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express()  // server
+const server = http.createServer(app) // http-server
+const io = socketio(server)   // socket with http-server
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -16,24 +18,39 @@ const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log('server started'))
 
+const botName = 'ChatCord Bot'
+
 //Run when client connects
 io.on('connection', socket => {
-   // Welcome current user
-    socket.emit('message', 'Welcome to ChatRoom')
+    socket.on('joinRoom', ({
+        username,
+        room
+    }) => {
+
+        const user = userJoin(socket.id, username, room)
+        
+        socket.join(user.room)
+
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatRoom'))
 
 
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat')
+        // Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', `${user.username} has joined the chat`)
 
-    //Runs when client disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+
     })
 
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
-       io.emit('message', msg)
-        
+        io.emit('message', formatMessage('USER', msg))
+
+    })
+
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'))
     })
 })
